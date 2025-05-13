19cb41a692af419d3df59e6086c4eef064de2f0d
KulwinderGurna <kulwinderkaurit@gmail.com>
2025-05-13 13:51:42 -0600

    update

---
+++ beautifulsoup.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.service import Service
+import csv
+
+
+#initialize the chrome web driver
+driver = webdriver.chrome()
+
+page_url = 'https://ww150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810000401'
+
+
+#fetching  the web page 
+driver.get(page_url)
+
+
+# Wair for the table to load
+driver.implicity_wait(10)
+
+# Get the page source after javaScript execution 
+page_source = driver.page_source
+
+# Parse the HTML content
+soup = BeautifulSoup(page_source, 'html.parser')
+
+# Find the table element 
+table = soup.find(id='simpleTable')
+
+if table:
+    #Extract column headers
+    headers = []
---
+++ python_examples.py
@@ -0,0 +1,22 @@
+# this is a comment 
+x = 5
+print(x)
+
+# this is an example of a tuple
+mytuple = ('honda', 'toyota', 'ford') 
+
+#lists (arrays)
+list  = ['apple', 'cherry', 'orange']
+
+#data dictionaries.key-value or object like collections
+dictionary = {'Todd': 38, 'John': 21, 'Mary': 38}
+
+#if-else conditional logic
+a = 33
+b = 220
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("b is greater than a")
+
+    
