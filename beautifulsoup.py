68a4983816b96e01cd7da9c36cf5676d25b5e9de
KulwinderGurna <kulwinderkaurit@gmail.com>
2025-05-14 15:21:39 -0600

    update

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver # type: ignore
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
 import csv
--- python_examples.py
+++ python_examples.py
@@ -15,8 +15,33 @@ dictionary = {'Todd': 38, 'John': 21, 'Mary': 38}
 a = 33
 b = 220
 if a > b:
-    print("a is greater than b")
+    print('a is greater than b')
 elif a < b:
-    print("b is greater than a")
+    print('b is greater than a')
+else:
+    print('a is equal to b')
+
+    # looping in python
+    colors = ['red', 'green', 'blue']
+for x in colors:
+    print(x)
+
+    #while loop
+    i = 1
+while i < 10:
+    print(i)
+    i += 1
+
+    #functions 
+    def add(a,b):
+        return a + b
+    
+    add(14, 5)
+    print(add(14, 5))
+#  casting
+x = int(1)
+b = float(2.33)
+v = str(23)
+
 
     
